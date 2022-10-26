738b9a9ffa0642f8db4ce2f31ce2757bec9e9cfd
nt-soumyadeep <soumyadeep@neurapses.com>
2022-10-26 18:42:04 +0530

    Fully working application

--- src/Components/DropdownMenu.js
+++ src/Components/DropdownMenu.js
@@ -4,29 +4,60 @@ import "./DropdownMenu.css";
 function DropdownMenu({ onSetType }) {
   return (
     <div className="dropdownMenu">
-      <div className="dropdownMenu__items" onClick={() => onSetType("")}>
-        AllNews
-      </div>
+      <button className="dropbtn">Filter</button>
+      <div className="dropdown-content">
+        <div className="dropdownMenu__items" onClick={() => onSetType("")}>
+          AllNews
+        </div>
 
-      <div
-        className="dropdownMenu__items"
-        onClick={() => onSetType("category=business&")}
-      >
-        Business
-      </div>
+        <div
+          className="dropdownMenu__items"
+          onClick={() => onSetType("category=business&")}
+        >
+          Business
+        </div>
 
-      <div
-        className="dropdownMenu__items"
-        onClick={() => onSetType("category=health&")}
-      >
-        Health
-      </div>
+        <div
+          className="dropdownMenu__items"
+          onClick={() => onSetType("category=health&")}
+        >
+          Health
+        </div>
 
-      <div
-        className="dropdownMenu__items"
-        onClick={() => onSetType("category=sports&")}
-      >
-        Sports
+        <div
+          className="dropdownMenu__items"
+          onClick={() => onSetType("category=sports&")}
+        >
+          Sports
+        </div>
+
+        <div
+          className="dropdownMenu__items"
+          onClick={() => onSetType("category=entertainment&")}
+        >
+          Entertainment
+        </div>
+
+        <div
+          className="dropdownMenu__items"
+          onClick={() => onSetType("category=general&")}
+        >
+          General
+        </div>
+
+        <div
+          className="dropdownMenu__items"
+          onClick={() => onSetType("category=science&")}
+        >
+          Science
+        </div>
+
+        <div
+          className="dropdownMenu__items"
+          onClick={() => onSetType("category=technology&")}
+        >
+          Technology
+        </div>
       </div>
     </div>
   );
--- src/Components/NewsList.js
+++ src/Components/NewsList.js
@@ -1,34 +1,54 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import './NewsList.css'
 
 function NewsList(props) {
   const [posts, setPosts] = useState([]);
+
+  const onSetPosts = (array) => {
+    setPosts(array);
+  };
+
+  // API call using async await
+  useEffect(() => {
+    async function getData() {
+      const res = await axios.get(props.url);
+      console.log(res)
+      const data = await res.data;
+      onSetPosts(res.data.sources);
+    }
+    getData();
+  }, [props.url]);
+
+// API call using promise
+/*
   useEffect(() => {
     axios
       .get(props.url)
       .then((res) => {
-        console.log(res.data.sources);
-        console.log("Inside Then");
         if (res && res.data && res.data.sources) {
-          setPosts(res.data.sources);
+            onSetPosts(res.data.sources);
         }
+        else{onSetPosts([]);}
       })
       .catch((err) => {
-        console.log("Inside Error", props.url);
         console.log(err);
       });
   }, [props.url]);
+*/
 
   return (
     <div className="newsList">
-      <div>{props.url}</div>
-      <div>News will appear here</div>
+      <h2>News will appear here</h2>
       <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            {post.name} | <b>{post.category}</b> | {<a href={post.url}>Link</a>}
-          </li>
-        ))}
+        {posts && posts.length > 0
+          ? posts.map((post) => (
+              <li key={post.id}>
+                {post.name} | <b>{post.category}</b> |{" "}
+                {<a href={post.url}>Link</a>}
+              </li>
+            ))
+          : null}
       </ul>
     </div>
   );
